1a2e9a9a2be4b4bd737709168f02e9f2cb6e8f19
jeffpatriota <jefferson.patriota@outlook.com>
2022-09-02 21:50:36 -0300

    add createMuiTheme function in App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
-import { Button } from '@material-ui/core';
+import { Button, ThemeProvider, createMuiTheme } from '@material-ui/core';
+import { orange, red, yellow } from "@material-ui/core/colors";
 
 function App() {
-  return <Button color="primary">Hello World</Button>;
+const theme = createMuiTheme({
+ palette: {
+  primary: {
+    main: '#f44336'
+  }
+ }
+})
+    
+  return (
+    <ThemeProvider theme={theme}>
+      <Button variant="contained" color="primary">Hello World</Button>
+    </ThemeProvider>
+  );
 }
 
 export default App;
